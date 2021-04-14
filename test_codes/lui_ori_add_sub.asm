e037463d146d50201c5f0a85de5b94c47f42d295
LeeDua <1063809383@qq.com>
2021-04-14 21:33:58 +0800

    ori test passed

---
+++ test_codes/lui_ori_add_sub.asm
@@ -0,0 +1,12 @@
+lui $t0 12
+lui $t1 34
+addu $s1 $t1 $t2
+lui $t2 56
+lui $t3 78
+subu $s2 $t3 $t2
+lui $t4 0x1234
+ori $t5 $t4 0x87
+ori $s3 $s2 0x65
+subu $s4 $s3 $s2
+addu $s5 $s4 $s1 
+ori $s6 $s5 0x43
